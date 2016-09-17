882acf721c717123ac1f4731bda2447d6e05b313
Lance Douglas <ldouglastech@gmail.com>
2016-09-17 12:19:56 -0400

    Online

--- src/PeriodicGame.java
+++ src/PeriodicGame.java
@@ -1,5 +1,9 @@
 public class PeriodicGame {
     public static void main(String[] args) {
+        String[] = {"this",}
+
+
+        System.out.println("This is a game designed to help players learn the periodic table.");
 
     }
 }
