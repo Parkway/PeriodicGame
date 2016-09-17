508389665b04edc668df3108e027bdaef7686813
Lance Douglas <ldouglastech@gmail.com>
2016-09-17 18:48:30 -0400

    Absolutely rebuilt it. Totally new function. Fully fledged chem game now.

--- src/PeriodicGame.java
+++ src/PeriodicGame.java
@@ -3,37 +3,60 @@ import java.util.Scanner;
 public class PeriodicGame {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int guessCount = 15;
-        String[] elementList = {"Hydrogen","Helium","Lithium","Beryllium","Boron","Carbon","Nitrogen","" +
-        "Oxygen","Fluorine","Neon","Sodium","Magnesium","Aluminum","Silicon","Phosphorous","Sulfur","" +
-        "Chlorine","Argon","Potassium","Calcium","Scandium","Titanium","Vanadium","Chromium","Manganese","" +
-        "Iron","Cobalt","Nickel","Copper","Zinc","Gallium","Germanium","Arsenic","Selenium","Bromine","" +
-        "Krypton","Rubidium","Strontium","Yttrium","Zirconium","Niobium","Molybdenum","Technetium","Ruthenium","" +
-        "Rhodium","Palladium","Silver","Cadmium","Indium","Tin","Antimony","Tellurium","Iodine","Xenon","" +
-        "Cesium","Barium","Lanthanum","Cerium","Praseodymium","Neodymium","Promethium","Samarium","Europium","" +
-        "Gadolinium","Terbium","Dysprosium","Holmium","Erbium","Thulium","Ytterbium","Lutetium","Hafnium","" +
-        "Tantalum","Tungsten","Rhenium","Osmium","Iridium","Platinum","Gold","Mercury","Thallium","Lead","" +
-        "Bismuth","Polonium","Astatine","Radon","Francium","Radium","Actinium","Thorium","Protactinium","" +
-        "Uranium","Neptunium","Americium","Curium","Berkelium","Californium","Einsteinium","Fermium","" +
-        "Mendelevium","Nobelium","Lawrencium","Rutherfordium","Dubnium","Seaborgium","Bohrium","Hassium","" +
-        "Meitnerium","Darmstadtium","Roentgenium","Ununbium","Ununtrium","Ununquadium","Ununpentium","" +
-        "Ununhexium","Ununseptium","Ununoctium"};
+        int guessCount = 3;
+        String[] elementList = {"Testhere", "Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "" +
+                "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium", "Aluminum", "Silicon", "Phosphorous", "Sulfur", "" +
+                "Chlorine", "Argon", "Potassium", "Calcium", "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese", "" +
+                "Iron", "Cobalt", "Nickel", "Copper", "Zinc", "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "" +
+                "Krypton", "Rubidium", "Strontium", "Yttrium", "Zirconium", "Niobium", "Molybdenum", "Technetium", "Ruthenium", "" +
+                "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin", "Antimony", "Tellurium", "Iodine", "Xenon", "" +
+                "Cesium", "Barium", "Lanthanum", "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europium", "" +
+                "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium", "Lutetium", "Hafnium", "" +
+                "Tantalum", "Tungsten", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead", "" +
+                "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Thorium", "Protactinium", "" +
+                "Uranium", "Neptunium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "" +
+                "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "" +
+                "Meitnerium", "Darmstadtium", "Roentgenium", "Copernicium", "Nihonium", "Flerovium", "Moscovium", "" +
+                "Livermorium", "Tennessine", "Oganesson"};
 
         int elementLength = elementList.length;
         int elementNumber = (int) (Math.random() * elementLength);
         String elementName = elementList[elementNumber];
-        System.out.println("This program randomly selects an element. You have to guess which element it is.\n" +
-                "You have 15 chances to get it, or you lose. Good Luck.");
-        while (guessCount > 0) {
-            guessCount--;
-            int elementGuess = scn.nextInt();
-            if (elementGuess == elementNumber) {
-                System.out.println("You got it! " + elementNumber + " is the atomic number for " + elementName + ".");
-            } else if (elementGuess > elementNumber) {
-                System.out.println("Too high, try again.");
-            } else if (elementGuess < elementNumber) {
-                System.out.println("Too low, guess again.");
+        System.out.println("Which do you need practice with, Atomic Numbers, or Element Names?");
+        String gameType = scn.nextLine();
+        if (gameType.toLowerCase().contains("atom".toLowerCase())) {
+            System.out.println("In this game, you have to enter the atomic number of the hidden element to win.\nYou have three tries. Good luck."
+                    + "\nThe element is: " + elementName);
+            while (guessCount > 0) {
+                guessCount--;
+                int numGuess = scn.nextInt();
+                if(numGuess == elementNumber) {
+                    System.out.println("You got it! The atomic number for " + elementName + " is " + elementNumber + "!");
+                    System.exit(0);
+                } else {
+                    System.out.println("Not quite. You have " + guessCount + " guesses remaining.");
+                } if (guessCount == 0) {
+                    System.out.println("You lose. The correct answer was: " + elementNumber);
+                }
             }
+        } else if (gameType.toLowerCase().contains("ele".toLowerCase())) {
+            System.out.println("In this game, you need to enter the name of the element represented by the atomic number to win.\nYou have three tries. Good luck."
+                    + "\nThe atomic number is: " + elementNumber);
+            while(guessCount > 0) {
+                guessCount--;
+                String eleGuess = scn.nextLine();
+                if(eleGuess.toLowerCase().equals(elementName.toLowerCase())) {
+                    System.out.println("You won! The Element with Atomic Number " + elementNumber + " is " + elementName);
+                    System.exit(0);
+                } else {
+                    System.out.println("Not quite. " + guessCount + " guesses left.");
+                } if (guessCount == 0) {
+                    System.out.println("You lose. The correct answer was: " + elementName
+                    + "\nYou entered: " + eleGuess);
+                }
+            }
+        } else {
+            System.out.println(gameType + " is not an option. Try again.");
         }
     }
 }
