34bf3477076209c952586f370ebc87a8dc292322
Lance Douglas <ldouglastech@gmail.com>
2016-09-18 13:55:44 -0400

    Added recursion for multiple games. Fixed up buggy results.

--- src/PeriodicGame.java
+++ src/PeriodicGame.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class PeriodicGame {
     public static void main(String[] args) {
+        game();
+    }
+    private static void game() {
         Scanner scn = new Scanner(System.in);
         int guessCount = 3;
-        String[] elementList = {"Testhere", "Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "" +
+        String[] elementList = {"AddOneium","Hydrogen","Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "" +
                 "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium", "Aluminum", "Silicon", "Phosphorous", "Sulfur", "" +
                 "Chlorine", "Argon", "Potassium", "Calcium", "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese", "" +
                 "Iron", "Cobalt", "Nickel", "Copper", "Zinc", "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "" +
@@ -14,34 +17,49 @@ public class PeriodicGame {
                 "Gadolinium", "Terbium", "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium", "Lutetium", "Hafnium", "" +
                 "Tantalum", "Tungsten", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead", "" +
                 "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Thorium", "Protactinium", "" +
-                "Uranium", "Neptunium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "" +
+                "Uranium", "Neptunium","Plutonium","Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "" +
                 "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "" +
                 "Meitnerium", "Darmstadtium", "Roentgenium", "Copernicium", "Nihonium", "Flerovium", "Moscovium", "" +
                 "Livermorium", "Tennessine", "Oganesson"};
 
         int elementLength = elementList.length;
         int elementNumber = (int) (Math.random() * elementLength);
+        System.out.println("There are " + (elementLength - 1) + " elements. How many do you know?");
         String elementName = elementList[elementNumber];
+
         System.out.println("Which do you need practice with, Atomic Numbers, or Element Names?");
         String gameType = scn.nextLine();
         if (gameType.toLowerCase().contains("atom".toLowerCase()) || gameType.toLowerCase().contains("num".toLowerCase())) {
-            System.out.println("In this game, you have to enter the atomic number of the hidden element to win.\nYou have " + guessCount + " tries. Good luck."
+            // ATOMIC GAME
+            System.out.println("In this game, you have to enter the atomic number of the element to win.\nYou have " + guessCount + " tries. Good luck."
                     + "\nThe element is: " + elementName);
-
             while (guessCount > 0) {
                 guessCount--;
                 int numGuess = scn.nextInt();
                 if(numGuess == elementNumber) {
                     System.out.println("You got it! The atomic number for " + elementName + " is " + elementNumber + "!");
-                    System.exit(0);
+                    System.out.println("Would you like to play again? Enter [1] for yes, and [2] for no.");
+                    int again = scn.nextInt();
+                    if (again == 1) {
+                        game();
+                    } else if (again == 2) {
+                        System.exit(0);
+                    }
                 } else {
                     System.out.println("Not quite. You have " + guessCount + " guesses remaining.");
                 } if (guessCount == 0) {
                     System.out.println("You lose. The correct answer was: " + elementNumber);
+                    System.out.println("Would you like to play again? Enter [1] for yes, and [2] for no.");
+                    int again = scn.nextInt();
+                    if (again == 1) {
+                        game();
+                    } else if (again == 2) {
+                        System.exit(0);
+                    }
                 }
             }
         } else if (gameType.toLowerCase().contains("ele".toLowerCase()) || gameType.toLowerCase().contains("nam")) {
-
+            // ELEMENT GAME
             System.out.println("In this game, you need to enter the name of the element represented by the atomic number to win.\nYou have " + guessCount + " tries. Good luck."
                     + "\nThe atomic number is: " + elementNumber);
             while(guessCount > 0) {
@@ -49,16 +67,30 @@ public class PeriodicGame {
                 String eleGuess = scn.nextLine();
                 if(eleGuess.toLowerCase().equals(elementName.toLowerCase())) {
                     System.out.println("You won! The Element with Atomic Number " + elementNumber + " is " + elementName + ".");
-                    System.exit(0);
+                    System.out.println("Would you like to play again?");
+                    String again = scn.nextLine();
+                    if (again.toLowerCase().contains("y".toLowerCase())) {
+                        game();
+                    } else if (again.toLowerCase().contains("n".toLowerCase())) {
+                        System.exit(0);
+                    }
                 } else {
                     System.out.println("Not quite. " + guessCount + " guesses left.");
                 } if (guessCount == 0) {
                     System.out.println("You lose. The correct answer was: " + elementName
                     + "\nYou entered: " + eleGuess);
+                    System.out.println("Would you like to play again? Enter 1 for yes and 2 for no.");
+                    int again = scn.nextInt();
+                    if (again == 1) {
+                        game();
+                    } else if (again == 2) {
+                        System.exit(0);
+                    }
                 }
             }
         } else {
             System.out.println(gameType + " is not an option. Try again.");
+            game();
         }
     }
 }
